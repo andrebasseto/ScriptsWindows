cb0a8dbc0a432025b08f433f0456e5d4a2396781
andrebasseto <39144357+andrebasseto@users.noreply.github.com>
2020-05-29 11:55:52 -0300

    Update Backup_7zip_Robocopy.bat

--- Backup_7zip_Robocopy.bat
+++ Backup_7zip_Robocopy.bat
@@ -9,7 +9,7 @@ REM MSG * Inicio da compactacao
 for %%X in (*.bak) do "%ProgramFiles%\7-Zip\7z.exe" -tzip -mmt=off -m0=lzma -mx3 a "%%X.zip" "%%X"
 
 REM MSG * Movendo arquivos
-REM COPIA OS ARQUIVOS DA ORIEGEM PARA DESTINO
+REM COPIA OS ARQUIVOS DA ORIGEM PARA DESTINO
 robocopy "E:\Backup\Diario Full" "\\SERVIDORDESTINO\Backup" *.zip /w:5 /r:5 /b /j /mov
 
 REM EXCLUIR OS ARQUIVO DE BAK SEM COMPACTAÇÃO
